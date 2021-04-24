c62f61b22e97d29695ff997a602a9eddc388e1ef
8bitgentleman <mainmoniker@googlemail.com>
2021-04-24 17:24:22 -0400

    feat:start to parse article info

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pocket2pdf
  downloads your saved articles from getPocket.com and converts them into a printable PDF
+
+## Setup
+  1. start up a virtual environment (_optional_)
+  2. install [karlicoss/pockexport](https://github.com/karlicoss/pockexport)
+
+  ```pip3 install --user git+https://github.com/karlicoss/pockexport```
+
+  3. change ```config_example.ini``` to ```config.ini```
+
+  4. Input your pocket info into ```config.ini```
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import pprint
 import pockexport.export as px
+import pockexport.dal as dal
+from os import listdir, path
 import configparser
 from datetime import datetime
 import os
@@ -16,20 +18,83 @@ POCKET_CONSUMER_KEY = str(config['Pocket']['CONSUMER_KEY'])
 POCKET_ACCESS_TOKEN = str(config['Pocket']['ACCESS_TOKEN'])
 
 
+def list_files_of_specific_format(directory, extension=None):
+    '''list all files of a specific extension from a directory'''
+    specific_files = []
+
+    for file in listdir(directory):
+        if extension:
+            if file.endswith("." + extension):
+                specific_files.append(path.join(directory, file))
+                # print(path.basename(file).split(' - ')[0])
+        else:
+            specific_files.append(path.join(directory, file))
+    specific_files = sorted(specific_files)
+    return specific_files
+
+
 def save_file(pocket_data):
-    if not os.path.exists('exports'):
-        os.makedirs('exports')
+    if not os.path.exists('pocket_exports'):
+        os.makedirs('pocket_exports')
     timestamp = int(datetime.timestamp(datetime.now()))
-    out_filename = 'exports/pocket-' + str(timestamp) + ".json"
+    out_filename = 'pocket_exports/pocket-' + str(timestamp) + ".json"
     with open(out_filename, 'w+') as f:
-        # todo sort by block/
+
         json.dump(pocket_data, f, indent=4, separators=(',', ': '), ensure_ascii=False)
+
+
+def pocket_archived():
+    pocket_path = 'pocket_exports/'
+
+    fil = list_files_of_specific_format(pocket_path, 'json')
+    # access the article info
+    d = dal.DAL(fil)
+    archived = []
+    for a in d.articles():
+        if a.json['status'] == '0':
+            archived.append(a.json)
+    # sort by time added
+    archived = sorted(archived, key=lambda k: k.get('time_added', 0), reverse=True)
+    for a in archived:
+        # omit tags I don't want to print
+        if 'tags' in a:
+            if "news" in a['tags']:
+                continue
+            if "covid19" in a['tags']:
+                continue
+            if "politics" in a['tags']:
+                continue
+        # if a['resolved_title'] is '':
+        #     print(a['given_title'])
+        # else:
+        #     print(a['resolved_title'])
+
+        title = a['resolved_title']
+        resolved_url = a['resolved_url']
+        excerpt = a['excerpt']
+        try:
+            tags = list(a['tags'].keys())
+        except Exception:
+            pass
+        try:
+            authors = []
+            pp.pprint(a['authors'])
+            for n in a['authors']:
+                # pp.pprint(n)
+                authors.append(n['name'])
+            print(authors)
+        except Exception:
+            pass
+        # # authors = a['authors']
+        # # domain = a['domain_metadata']['name']
+        # # top_image = a['top_image_url']
 
 
 def main():
     # first export and save your pocket info
-    pocket_data = px.get_json(consumer_key=POCKET_CONSUMER_KEY, access_token=POCKET_ACCESS_TOKEN)
-    save_file(pocket_data)
+    # pocket_data = px.get_json(consumer_key=POCKET_CONSUMER_KEY, access_token=POCKET_ACCESS_TOKEN)
+    # save_file(pocket_data)
+    pocket_archived()
 
 
 if __name__ == '__main__':
